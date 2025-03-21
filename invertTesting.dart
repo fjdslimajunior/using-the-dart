9d3fbb1eb01128b804f6fd3bc555855ae405804f
Lima Junior <fjdlimajunior@gmail.com>
2025-03-20 23:36:32 -0300

    update in performance

--- invertTesting.dart
+++ invertTesting.dart
@@ -12,10 +12,14 @@ Dica: Em Dart, você pode usar .split('').reversed.join('').
 
 main() {
   //entrada
+  print(
+    'Esse programa irá converrter o seu texto. \n O intuito é simples e serve para \n aperfeiçoamento dos aprendizados básicos em Dart.',
+  );
   print('Informe o valor de Entrada: ');
   var entrada = stdin.readLineSync() ?? '';
 
   //saída
   var saida = entrada.split('').reversed.join();
+
   print('Texto invertido: $saida');
 }
